356241cee81208da17b84b6c51c98bd56bb10fce
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2017-01-18 19:17:58 +0000

    Creating and printing empty images

--- BitmapEditor.rb
+++ BitmapEditor.rb
@@ -2,6 +2,8 @@ class BitmapEditor
 
   # Initialise the class as the program hasn't started yet
   @running = false
+  @image = nil
+  BLANK_PIXEL = 'O'
 
   # Start the program running
   # @public
@@ -9,6 +11,7 @@ class BitmapEditor
   def start
     
     @running = true
+    puts 'Welcome to Bitmap Editor'
     puts 'type ? for help'
 
     # Keep looping until the user ends the program
@@ -29,6 +32,8 @@ class BitmapEditor
           show_help
         when 'I'
           new_image(args)
+        when 'S'
+          show_image
         when 'X'
           exit_console
         else
@@ -46,7 +51,42 @@ class BitmapEditor
     # @params args - The array of string arguments
     # @return void
     def new_image(args)
-      puts args
+      
+      # Convert the width and height arguments to integer
+      width = args[0].to_i
+      height = args[1].to_i
+      
+      # Create a new 2-dimensional array
+      @image = Array.new(height) { Array.new(width) }
+      clear_image
+
+    end
+
+    # Replaces all the pixels in the image by an O
+    # @private
+    # @return void
+    def clear_image
+      for y in 0..(@image.length-1)
+        for x in 0..(@image[y].length-1)
+          @image[y][x] = BLANK_PIXEL
+        end
+      end
+    end
+
+    # Prints the image to the screen
+    # @private
+    # @return void
+    def show_image
+      if @image == nil
+        puts "You haven't created an image yet"
+      else
+        for y in 0..(@image.length-1)
+          for x in 0..(@image[y].length-1)
+            print @image[y][x]
+          end
+          print "\n"
+        end
+      end
     end
 
     # Exits the program
@@ -62,7 +102,7 @@ class BitmapEditor
     # @return void
     def show_help
       puts "I M N - Create a new M x N image. \n" +
-           "C - Sets all pixels to colour 'O'. \n" +
+           "C - Sets all pixels to colour '" + BLANK_PIXEL + "'. \n" +
            "L X Y C - Colours the pixel (X,Y) with colour C. \n" +
            "V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive). \n" +
            "H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive). \n" +

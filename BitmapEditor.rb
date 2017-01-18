08ef64d765acc988e545e02b0cd21fff0efa677a
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2017-01-18 10:21:03 +0000

    Start handling inputs, using Utils class

--- BitmapEditor.rb
+++ BitmapEditor.rb
@@ -1,36 +1,73 @@
 class BitmapEditor
 
-  def run
+  # Initialise the class as the program hasn't started yet
+  @running = false
+
+  # Start the program running
+  # @public
+  # @return void
+  def start
+    
     @running = true
     puts 'type ? for help'
+
+    # Keep looping until the user ends the program
     while @running
+      
       print '> '
+      
+      # Get the input from the user and switch on the first character
       input = gets.chomp
-      case input
+      first_char = input[0, 1].capitalize
+
+      # Call a utiltity function to break down the white-spaced inputs
+      # into an array of strings
+      args = Utils.analyse_input(input)
+
+      case first_char
         when '?'
           show_help
+        when 'I'
+          new_image(args)
         when 'X'
           exit_console
         else
-          puts 'unrecognised command :('
+          puts 'Unrecognised command "' + input + '"'
       end
+
     end
+
   end
 
   private
+    
+    # Create a new bitmap in the class
+    # @private
+    # @params args - The array of string arguments
+    # @return void
+    def new_image(args)
+      puts args
+    end
+
+    # Exits the program
+    # @private
+    # @return void
     def exit_console
-      puts 'goodbye!'
+      puts "Thanks for running the program!"
       @running = false
     end
 
+    # Prints help text
+    # @private
+    # @return void
     def show_help
-      puts "? - Help
-I M N - Create a new M x N image with all pixels coloured white (O).
-C - Clears the table, setting all pixels to white (O).
-L X Y C - Colours the pixel (X,Y) with colour C.
-V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
-H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
-S - Show the contents of the current image
-X - Terminate the session"
+      puts "I M N - Create a new M x N image. \n" +
+           "C - Sets all pixels to colour 'O'. \n" +
+           "L X Y C - Colours the pixel (X,Y) with colour C. \n" +
+           "V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive). \n" +
+           "H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive). \n" +
+           "S - Show the contents of the current image \n" +
+           "X - Terminate the session"
     end
+
 end
--- Main.rb
+++ Main.rb
@@ -1,2 +1,7 @@
 require './BitmapEditor'
-BitmapEditor.new.run
+require './Utils'
+
+# Create a new instance of the bitmap editor
+# and start the program
+b = BitmapEditor.new
+b.start

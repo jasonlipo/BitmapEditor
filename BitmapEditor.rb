a69a35c205e64b2a502922149f235600541fbfc6
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2017-01-18 20:43:45 +0000

    Ensure correct number of arguments follow command

--- BitmapEditor.rb
+++ BitmapEditor.rb
@@ -32,23 +32,28 @@ class BitmapEditor
       # into an array of strings
       args = Utils.analyse_input(input)
 
-      case first_char
-        when '?'
-          show_help
-        when 'I'
-          new_image(args)
-        when 'S'
-          show_image
-        when 'C'
-          clear_image
-        when 'L'
-          set_pixel_colour(args)
-        when 'H', 'V'
-          draw_line(first_char, args)
-        when 'X'
-          exit_console
-        else
-          puts 'Unrecognised command "' + input + '"'
+      # Check the correct number of arguments
+      if Utils.number_args(first_char, args)
+
+        case first_char
+          when '?'
+            show_help
+          when 'I'
+            new_image(args)
+          when 'S'
+            show_image
+          when 'C'
+            clear_image
+          when 'L'
+            set_pixel_colour(args)
+          when 'H', 'V'
+            draw_line(first_char, args)
+          when 'X'
+            exit_console
+          else
+            puts 'Unrecognised command "' + input + '"'
+        end
+
       end
 
     end
@@ -62,7 +67,7 @@ class BitmapEditor
     # @param args - The array of string arguments
     # @return void
     def new_image(args)
-      
+
       # Convert the width and height arguments to integer
       @width = args[0].to_i
       @height = args[1].to_i

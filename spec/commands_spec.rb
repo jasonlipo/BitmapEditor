8ecee6e41d3b2638b60aab9c4990ad6f95941a7a
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-22 15:05:54 +0000

    Add contexts for Commands.input

--- spec/commands_spec.rb
+++ spec/commands_spec.rb
@@ -7,6 +7,16 @@ require_relative "../BitmapEditor"
 describe Commands do
 
   describe "#inputs" do
+    
+    context "empty list" do
+    end
+
+    context "multiple arguments" do
+    end
+
+    context "single argument" do
+    end
+
   end
 
 end

977cd6e317d9b66b6627c700e6de0b819ccefb8c
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-22 11:14:18 +0000

    Separate out tests for sending in valid input

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -20,24 +20,23 @@ describe BitmapEditor do
     end
 
     context "when sending in valid input" do
+      
+      before(:each) do
+        @output = @bitmap.process("I 5 6")
+      end
+      
+      it "should not return any output" do
+        expect(@output).to be_empty
+      end
+      
       it "should have the correct image dimensions" do
-        message = @bitmap.process("I 5 6")
-        expect(message).to be_empty
         im = @bitmap.get_image
         expect(im.getWidth).to eq 5
         expect(im.getHeight).to eq 6
       end
+
     end
-    
+
   end
 
 end
-
-# assert_equal("", x.process("I 5 6"))
-# assert_equal("OOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO", x.process("S"))
-# assert_equal("The image must be at minimum 1x1", x.process("I 0 9"))
-# assert_equal("X and Y must both be numbers", x.process("I B 9"))
-# assert_equal("You should only enter 2 value(s) after the 'I'", x.process("I 8 7 2"))
-# assert_equal("The maximum width and height is 250", x.process("I 300 62"))
-# assert_equal("", x.process("I 3 3"))
-# assert_equal("OOO\nOOO\nOOO", x.process("S"))

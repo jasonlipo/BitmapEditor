978a18efe17cf0236845ad633aea9fc6911903e6
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-22 13:02:58 +0000

    When entering blank inputs

--- spec/io_spec.rb
+++ spec/io_spec.rb
@@ -13,6 +13,12 @@ describe BitmapEditor do
   describe "io" do
 
     context "empty inputs" do
+      it "should give you a prompt to enter something" do
+        prompt = @bitmap.process("")
+        expect(prompt).to eq "Please type a valid command, type ? for help"
+        prompt2 = @bitmap.process("   ")
+        expect(prompt).to eq "Please type a valid command, type ? for help"
+      end
     end
 
     context "wrong number of inputs" do

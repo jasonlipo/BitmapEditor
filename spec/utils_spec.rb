9a2436a09a8f0dd2481d42bcbe305d432a866419
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-22 14:35:12 +0000

    Add test spec for Utils class

---
+++ spec/utils_spec.rb
@@ -0,0 +1,44 @@
+require_relative "../IOManager"
+require_relative "../Commands"
+require_relative "../Image"
+require_relative "../Utils"
+require_relative "../BitmapEditor"
+
+describe Utils do
+
+  describe "#is_uppercase_char" do
+    
+    context "try number" do
+    end
+
+    context "try lowercase letter" do
+    end
+
+    context "try uppercase letter" do
+    end
+
+    context "try special character" do
+    end
+
+    context "try mixture of characters" do
+    end
+
+  end
+
+  describe "#is_numeric" do
+    
+    context "empty list" do
+    end
+
+    context "list of numeric strings" do
+    end
+
+    context "list of numbers" do
+    end
+
+    context "mixture of numbers and words" do
+    end
+
+  end
+
+end

8b19685ac658efec31a8c0004509ffdd8174d5d6
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-22 15:17:46 +0000

    When entering multiple arguments to Commands.input

--- spec/commands_spec.rb
+++ spec/commands_spec.rb
@@ -7,15 +7,28 @@ require_relative "../BitmapEditor"
 describe Commands do
 
   describe "#inputs" do
-    
+
+    before(:each) do
+      @c = Commands.new("", BitmapEditor.new(nil))
+    end
+
     context "empty input" do
       it "should be an empty list" do
-        c = Commands.new("", BitmapEditor.new(nil))
-        expect(c.inputs("")).to eq []
+        expect(@c.inputs("")).to eq []
       end
     end
 
     context "multiple arguments" do
+      it "should be a list split up by new lines" do
+        expect(@c.inputs("U 1 2 3 4 5")).to eq ["1", "2", "3", "4", "5"]
+        expect(@c.inputs("B K U P")).to eq ["K", "U", "P"]
+      end
+      
+      it "should should have one element missing" do
+        str = "U 1 2 3 4 5"
+        output = @c.inputs(str)
+        expect(str.strip.split(" ")[0]).to_not eq output[0]
+      end
     end
 
     context "single argument" do

397fe3ba7a2da59bde6642ff59d5ee43a554e914
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-22 14:45:40 +0000

    More specs for is_uppercase_char returning falsey values

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -18,15 +18,33 @@ describe Utils do
     end
 
     context "try lowercase letter" do
+      it "should return false" do
+        expect(Utils.is_uppercase_char("g")).to be_falsey
+        expect(Utils.is_uppercase_char("a")).to be_falsey
+        expect(Utils.is_uppercase_char("z")).to be_falsey
+        expect(Utils.is_uppercase_char("jh4ghs")).to be_falsey
+      end
     end
 
     context "try uppercase letter" do
     end
 
     context "try special character" do
+      it "should return false" do
+        expect(Utils.is_uppercase_char(".=")).to be_falsey
+        expect(Utils.is_uppercase_char("+[]")).to be_falsey
+        expect(Utils.is_uppercase_char("/hj")).to be_falsey
+        expect(Utils.is_uppercase_char(":SA")).to be_falsey
+      end
     end
 
     context "try mixture of characters" do
+      it "should return false" do
+        expect(Utils.is_uppercase_char("8j4f")).to be_falsey
+        expect(Utils.is_uppercase_char("Gmb3F")).to be_falsey
+        expect(Utils.is_uppercase_char("zdD3g")).to be_falsey
+        expect(Utils.is_uppercase_char("SSSSS")).to be_falsey
+      end
     end
 
   end

12f92717e288a303929194f5cfb632a5aadcc214
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-22 15:30:18 +0000

    Testing Image class, no need for class scope variable

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -8,46 +8,46 @@ describe Image do
 
   describe "#fill" do
     it "should replace every pixel with this new letter" do
-      @im = Image.new(5, 5)
-      @im.fill("A")
-      expect(@im.show).to eq "AAAAA\nAAAAA\nAAAAA\nAAAAA\nAAAAA"
-      @im.fill("S")
-      expect(@im.show).to eq "SSSSS\nSSSSS\nSSSSS\nSSSSS\nSSSSS"
+      im = Image.new(5, 5)
+      im.fill("A")
+      expect(im.show).to eq "AAAAA\nAAAAA\nAAAAA\nAAAAA\nAAAAA"
+      im.fill("S")
+      expect(im.show).to eq "SSSSS\nSSSSS\nSSSSS\nSSSSS\nSSSSS"
     end
   end
 
   describe "#line" do
     it "should draw either a horizontal or vertical line" do
-      @im = Image.new(3, 6)
-      @im.line("H", 1, 3, 4, "N")
-      expect(@im.show).to eq "OOO\nOOO\nOOO\nNNN\nOOO\nOOO"
-      @im = Image.new(7, 2)
-      @im.line("V", 4, 1, 2, "J")
-      expect(@im.show).to eq "OOOJOOO\nOOOJOOO"
+      im = Image.new(3, 6)
+      im.line("H", 1, 3, 4, "N")
+      expect(im.show).to eq "OOO\nOOO\nOOO\nNNN\nOOO\nOOO"
+      im = Image.new(7, 2)
+      im.line("V", 4, 1, 2, "J")
+      expect(im.show).to eq "OOOJOOO\nOOOJOOO"
     end
   end
 
   describe "#fits_width" do
     it "should return true if the value is within the width" do
-      @im = Image.new(4, 8)
-      expect(@im.fits_width(0)).to be true
-      expect(@im.fits_width(1)).to be true
-      expect(@im.fits_width(2)).to be true
-      expect(@im.fits_width(4)).to be true
-      expect(@im.fits_width(5)).to be false
-      expect(@im.fits_width(8)).to be false
+      im = Image.new(4, 8)
+      expect(im.fits_width(0)).to be true
+      expect(im.fits_width(1)).to be true
+      expect(im.fits_width(2)).to be true
+      expect(im.fits_width(4)).to be true
+      expect(im.fits_width(5)).to be false
+      expect(im.fits_width(8)).to be false
     end
   end
 
   describe "#fits_height" do
     it "should return true if the value is within the height" do
-      @im = Image.new(4, 8)
-      expect(@im.fits_height(0)).to be true
-      expect(@im.fits_height(4)).to be true
-      expect(@im.fits_height(5)).to be true
-      expect(@im.fits_height(8)).to be true
-      expect(@im.fits_height(9)).to be false
-      expect(@im.fits_height(10)).to be false
+      im = Image.new(4, 8)
+      expect(im.fits_height(0)).to be true
+      expect(im.fits_height(4)).to be true
+      expect(im.fits_height(5)).to be true
+      expect(im.fits_height(8)).to be true
+      expect(im.fits_height(9)).to be false
+      expect(im.fits_height(10)).to be false
     end
   end
 

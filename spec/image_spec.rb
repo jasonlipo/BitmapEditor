b618bfc32569f7236c9605af55b243bd9acfa4e4
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-22 15:25:39 +0000

    When drawing a vertical or horizontal line on the image

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -7,18 +7,24 @@ require_relative "../BitmapEditor"
 describe Image do
 
   describe "#fill" do
-    context "when entering a letter" do
-      it "should replace every pixel with this new letter" do
-        @im = Image.new(5, 5)
-        @im.fill("A")
-        expect(@im.show).to eq "AAAAA\nAAAAA\nAAAAA\nAAAAA\nAAAAA"
-        @im.fill("S")
-        expect(@im.show).to eq "SSSSS\nSSSSS\nSSSSS\nSSSSS\nSSSSS"
-      end
+    it "should replace every pixel with this new letter" do
+      @im = Image.new(5, 5)
+      @im.fill("A")
+      expect(@im.show).to eq "AAAAA\nAAAAA\nAAAAA\nAAAAA\nAAAAA"
+      @im.fill("S")
+      expect(@im.show).to eq "SSSSS\nSSSSS\nSSSSS\nSSSSS\nSSSSS"
     end
   end
 
   describe "#line" do
+    it "should draw either a horizontal or vertical line" do
+      @im = Image.new(3, 6)
+      @im.line("H", 1, 3, 4, "N")
+      expect(@im.show).to eq "OOO\nOOO\nOOO\nNNN\nOOO\nOOO"
+      @im = Image.new(7, 2)
+      @im.line("V", 4, 1, 2, "J")
+      expect(@im.show).to eq "OOOJOOO\nOOOJOOO"
+    end
   end
 
   describe "#fits_width" do

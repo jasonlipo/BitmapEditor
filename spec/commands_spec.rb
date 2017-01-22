8f974fa9987ea2f8022f2e5cd0403d32a8ab23ec
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-22 14:55:38 +0000

    Add remaining specs

---
+++ spec/commands_spec.rb
@@ -0,0 +1,12 @@
+require_relative "../IOManager"
+require_relative "../Commands"
+require_relative "../Image"
+require_relative "../Utils"
+require_relative "../BitmapEditor"
+
+describe Commands do
+
+  describe "#inputs" do
+  end
+
+end
---
+++ spec/image_spec.rb
@@ -0,0 +1,25 @@
+require_relative "../IOManager"
+require_relative "../Commands"
+require_relative "../Image"
+require_relative "../Utils"
+require_relative "../BitmapEditor"
+
+describe Image do
+
+  before(:each) do
+    @im = Image.new(10, 10)
+  end
+
+  describe "#fill" do
+  end
+
+  describe "#line" do
+  end
+
+  describe "#fits_width" do
+  end
+
+  describe "#fits_height" do
+  end
+
+end
